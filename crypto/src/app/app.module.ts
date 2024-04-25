d2897271680d2e70ce0f9b6621cc347a0650043c
Pengwin <andi.csaba@stud.u-szeged.hu>
2024-04-25 23:25:19 +0200

    Added firebase hosting to project

--- crypto/src/app/app.module.ts
+++ crypto/src/app/app.module.ts
@@ -3,6 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCtjwj8P27rT7tSRk03FzvuazdwCLFvcWw",
+
+  authDomain: "cryptokert.firebaseapp.com",
+
+  projectId: "cryptokert",
+
+  storageBucket: "cryptokert.appspot.com",
+
+  messagingSenderId: "905908001588",
+
+  appId: "1:905908001588:web:1ea56456eeb37151d62361"
+
+};
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +33,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
   ],
   providers: [],
   bootstrap: [AppComponent]
